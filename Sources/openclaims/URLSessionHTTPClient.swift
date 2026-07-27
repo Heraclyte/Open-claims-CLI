67bc7ee87d7d5aba261edf38b8d2e31327bdbdaf
gakill2 <inotexist34@protonmail.com>
2026-07-27 09:43:20 +0200

    Added URLSessionHTTPClient conforming to HTTPClientProtocol using ephemeral session configuration

---
+++ Sources/openclaims/URLSessionHTTPClient.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+#if canImport(FoundationNetworking)
+    import FoundationNetworking
+#endif
+
+public final class URLSessionHTTPClient: HTTPClientProtocol, @unchecked Sendable {
+    private let session: URLSession
+
+    public init(session: URLSession = URLSessionHTTPClient.createDefaultSession()) {
+        self.session = session
+    }
+
+    public static func createDefaultSession() -> URLSession {
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        configuration.urlCache = nil
+        return URLSession(configuration: configuration)
+    }
+    public func fetchClaimEnvelope(from url: URL) async throws -> ClaimEnvelope {
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+
+        let (data, response): (Data, URLResponse)
+
+        do {
+            (data, response) = try await session.data(for: request)
+        } catch {
+            throw HTTPClientError.invalidResponse
+        }
+
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw HTTPClientError.invalidResponse
+        }
+
+        guard (200...299).contains(httpResponse.statusCode) else {
+            throw HTTPClientError.requestFailed(statusCode: httpResponse.statusCode)
+        }
+
+        do {
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            return try decoder.decode(ClaimEnvelope.self, from: data)
+        } catch {
+            throw HTTPClientError.decodingError
+        }
+    }
+}
