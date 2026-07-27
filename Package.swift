cf63a43d3aafc066d09ea724be284e38fcbe9484
gakill2 <inotexist34@protonmail.com>
2026-07-27 13:48:18 +0200

    Updating dependencies for verification cryptography

--- Package.swift
+++ Package.swift
@@ -5,13 +5,15 @@ import PackageDescription
 let package = Package(
     name: "openclaims",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0")
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift\u{2D}crypto.git", from: "3.0.0"),
     ],
     targets: [
         .executableTarget(
             name: "openclaims",
             dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Crypto", package: "swift\u{2D}crypto"),
             ],
             path: "Sources"
         ),
