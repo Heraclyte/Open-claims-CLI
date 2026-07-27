533b218cedd98b35488748fb306f6161f9ea34b6
gakill2 <inotexist34@protonmail.com>
2026-07-27 10:34:06 +0200

    Implement LocalPDFMetadataReader to extract issuer URLs from raw file data

---
+++ Sources/openclaims/LocalPDFMetadataReader.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+public final class LocalPDFMetadataReader: MetadataReaderProtocol {
+    public init() {}
+
+    public func extractIssuerURL(fromFilePath path: String) async throws -> URL {
+        let fileURL = URL(fileURLWithPath: path)
+
+        guard let data = try? Data(contentsOf: fileURL) else {
+            throw MetadataReaderError.fileReadFailure
+        }
+
+        let content = String(decoding: data, as: UTF8.self)
+
+        guard let range = content.range(of: "OpenClaimsIssuer: ") else {
+            throw MetadataReaderError.metadataNotFound
+        }
+
+        let substring = content[range.upperBound...]
+        let components = substring.split(separator: " ")
+
+        guard let urlString = components.first,
+            let url = URL(string: String(urlString).trimmingCharacters(in: .whitespacesAndNewlines))
+        else {
+            throw MetadataReaderError.metadataNotFound
+        }
+
+        return url
+    }
+}
