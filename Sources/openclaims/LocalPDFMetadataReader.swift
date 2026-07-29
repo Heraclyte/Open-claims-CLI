eeab8776f2a3692aeb380d2fa2305b2783915303
gakill2 <inotexist34@protonmail.com>
2026-07-29 11:28:16 +0200

    parse PDF line by line and validate URL scheme

--- Sources/openclaims/LocalPDFMetadataReader.swift
+++ Sources/openclaims/LocalPDFMetadataReader.swift
@@ -11,15 +11,15 @@ public final class LocalPDFMetadataReader: MetadataReaderProtocol {
         }
 
         let content = String(decoding: data, as: UTF8.self)
-        let lines = content.components(separatedBy: .newlines)
+        let lines: [String] = content.components(separatedBy: .newlines)
 
         var foundURLString: String?
 
         for line in lines {
-
             if let range = line.range(of: "OpenClaimsIssuer: ") {
                 let substring = line[range.upperBound...]
-                let components = substring.split(separator: " ")
+                let components = substring.split(whereSeparator: { $0.isWhitespace || $0.isNewline }
+                )
                 if let firstComponent = components.first {
                     foundURLString = String(firstComponent).trimmingCharacters(
                         in: .whitespacesAndNewlines)
